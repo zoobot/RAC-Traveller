68745a8ba122debbe84cb6552c331ca657a6d065
botzoo <rommims@gmail.com>
2017-02-09 16:29:18 -0800

    api keys

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-consumer_key: "jfFV2jBcLwVwfqEHkBmY02eLd",
-consumer_secret: "c5U0HziWAr0ARNvaDiEOlvDbbATs8gNw3c14g6QNFiZyK27x64",
-access_token: "747836254255886337-g2OxYV6NUGGAEHcp63RGSFbvZuTLjhg",
-access_token_secret: "88xHpKlgKLGbMInmdR3YqR42Ei89DfhGLBrFcrxQd5Q6g",
+consumer_key: "YOUR KEYS HERE",
+consumer_secret: "YOUR KEYS HERE",
+access_token: "YOUR KEYS HERE",
+access_token_secret: "YOUR KEYS HERE",
 }
